6e0b26bdf1a8def327cdb165a78b10712298469a
SHLYAKHTIN DENIS <99750932+DrDrv@users.noreply.github.com>
2022-06-18 00:25:56 +0300

    Update import_export.py
    fix

--- import_export.py
+++ import_export.py
@@ -1,48 +1,37 @@
 def impexp_file(list_in):
+    from adr_func import show, add_persons, csv_to_list
     import os.path
-    
-    # Функция вывода на экран импортированных данных - Удалить при слиянии
-    def show_all(lst):
-        for item in lst:
-            print(f'|| {item[0][:15]:15} | {item[1]:15} | {item[2]:10} | {item[3]:10}')
+    from termcolor import colored
 
     print('Импорт/Экспорт файла контактов')
     file_exist = False
     while file_exist != True:
-        in_put = input('Импорт - 1 | Экспорт -2 | Выход - 3 \t (1-3):')
+        print(colored('| 1. Импорт | 2. Экспорт | 3. Выход','green'))
+        in_put = input(colored('Выберите действие: ','blue'))
         if in_put == '3': return # Выход из модуля
-        
         # Раздел обработки импорта файла
         if in_put == '1':
-            import_file = input(f'Введите имя файла для импорта: \t')
+            import_file = input(f'Введите имя файла для импорта: ')
             file_exist = os.path.exists(import_file)  # Проверка на существование файла
             if file_exist == True: 
-                print(f'{import_file} - файл найден...')
+                print(colored(f'{import_file} - файл найден...','green'))
                 print('Чтение файла')                              # - Удалить при слиянии
-                # import_file = from_csv_to_list(import_file)    # Получаем данные из файла
-                import_file = [['Ахметдянов','Ахмет','+79800000001','комит т1'],['Буржуев','Буржуй','+79800000002','комит т2']] # - Удалить при слиянии
-                show_all(import_file)                        # Показать пользователю импортированные данные
-                y_n = input(f'Выполнить импорт? (y/n): \t')
+                import_list = csv_to_list(import_file)    # Получаем данные из файла
+                show(4, import_file)                        # Показать пользователю импортированные данные
+                y_n = input(colored(f'Выполнить импорт? (y/n): ','blue'))
                 if y_n == 'y': 
-                    list_in += import_file
-                    print('Запись файла')                           # - Удалить при слиянии
-                    #from_list_to_csv(list_in, 0)  # Записываем данные в файл (если 0 - то файл по умолчанию, иначе указанный файл)
+                    list_in += import_list
+                    add_persons(list_in,'')  # Записываем данные в файл (если '' - то файл по умолчанию, иначе указанный файл)
             elif file_exist == False: print('Файл отсутствует!') # Возврат в меню модуля
         
         # Раздел обработки экспорта в файл
         if in_put == '2':
-            export_file = input(f'Введите имя файла для экспорта: \t')
+            export_file = input(f'Введите имя файла для экспорта: ')
             file_exist = os.path.exists(export_file)  # Проверка на существование файла
             if file_exist == True: 
-                print(f'{export_file} - файл существует! Перезаписать? (y/n): \t')
+                print(colored(f'{export_file} - файл существует! Перезаписать? (y/n): ','red'))
                 if y_n == 'y': 
-                    print('Запись файла')                           # - Удалить при слиянии
-                    #from_list_to_csv(list_in, export_file)  # # Записываем данные вуказанный  файл (если 0 - то файл по умолчанию)
+                    add_persons(list_in, export_file)  # # Записываем данные вуказанный  файл (если ' - то файл по умолчанию)
             elif file_exist == False:
-                print('Запись файла')                               # - Удалить при слиянии
                 file_exist = True
-                #from_list_to_csv(list_in, export_file)  # # Записываем данные вуказанный  файл (если 0 - то файл по умолчанию)
-        
-
-List_In = [['Иванов', 'Иван', '+79260000001','комментарий 1'], ['Петров', 'Петр', '+79260000002','комментарий 2'], ['Васильев', 'Василий', '+79260000003','комментарий 3'] ] # - Удалить при слиянии
-impexp_file(List_In)       
+                add_persons(list_in, export_file)  # # Записываем данные вуказанный  файл (если '' - то файл по умолчанию)
